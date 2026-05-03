14e268b6ca0cb07ecbbdd5b7e629897d98bec8f2
GabrielSyR20 <gabriel.benja.syr@gmail.com>
2026-05-03 14:12:02 -0300

    CositasInsanas

---
+++ TareaActualizada/registro.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @type {HTMLButtonElement}
+ */
+const registroBtn = document.querySelector(".ButtonRegistro");
+console.log(registroBtn);
+/**
+ * 
+ * @type {HTMLInputElement}
+ */
+const nombreInput = document.querySelector("#nombre");
+console.log(nombreInput);
